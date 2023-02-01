73a63c4d7760dc1441c78dbdf10dc05677540ea4
emrekardaslar <emre.kardaslar@gmail.com>
2023-02-01 19:48:54 +0300

    add removeFromFavoritesRepo

--- app/repository/favoritesRepository.ts
+++ app/repository/favoritesRepository.ts
@@ -6,3 +6,9 @@ export async function getFavoriteListRepo() {
     const res = await axios.get(API_BASE_URL + "/favoriteList", { headers: {"Authorization" : `Bearer ${localStorage.getItem("access")}`} });
     return res.data.results;
 }
+
+export async function removeFromFavoritesRepo(productId: string) {
+    const API_BASE_URL = getEnvVariable("API_URL")
+    const res = await axios.delete(API_BASE_URL + `/favoriteList/${productId}/`, { headers: {"Authorization" : `Bearer ${localStorage.getItem("access")}`} });
+    return res.data.results;
+}
